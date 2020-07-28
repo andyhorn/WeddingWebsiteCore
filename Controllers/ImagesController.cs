bb5b4649ef089b081f3f9dd6b16fe3de8d71c8e6
Andy Horn <andyjhorn@gmail.com>
2020-07-28 08:32:55 -0500

    Finshed the ImagesController

--- Controllers/ImagesController.cs
+++ Controllers/ImagesController.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using WeddingWebsiteCore.Contracts;
+using WeddingWebsiteCore.DataAccess;
+using WeddingWebsiteCore.Models;
 
 namespace WeddingWebsiteCore.Controllers
 {
@@ -11,5 +16,116 @@ namespace WeddingWebsiteCore.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly WeddingContext _context;
+        public ImagesController(WeddingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(RouteContracts.GetAll)]
+        public async Task<IActionResult> GetAllImages()
+        {
+            var images = await _context.Images.ToListAsync();
+
+            return Ok(images);
+        }
+
+        [HttpGet(RouteContracts.GetItem)]
+        public async Task<IActionResult> GetImage(int id)
+        {
+            var image = await _context.Images.FindAsync(id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(image);
+        }
+
+        [HttpGet(RouteContracts.PostItem)]
+        public async Task<IActionResult> PostNewImage([FromBody] Image image)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var existingImage = await _context.Images.FindAsync(image.ImageId);
+            if (existingImage != null)
+            {
+                return BadRequest(ErrorMessageContracts.IdConflict);
+            }
+
+            try
+            {
+                await _context.AddAsync(image);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(PostNewImage), image, image.ImageId);
+        }
+
+        [HttpPut(RouteContracts.PutItem)]
+        public async Task<IActionResult> UpdateImage(int id, [FromBody]Image image)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            if (id != image.ImageId)
+            {
+                return BadRequest(ErrorMessageContracts.MismatchedId);
+            }
+
+            var existingImage = await _context.Images.FindAsync(id);
+            if (existingImage == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Entry(image).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete(RouteContracts.DeleteItem)]
+        public async Task<IActionResult> DeleteImage(int id)
+        {
+            var image = await _context.Images.FindAsync(id);
+
+            if (image == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Remove(image);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
     }
 }
