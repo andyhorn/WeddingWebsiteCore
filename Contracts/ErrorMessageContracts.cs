a38d0c8f7564c3b2cd9981324aeba4ed20b7a649
Andy Horn <andyjhorn@gmail.com>
2020-07-28 08:31:47 -0500

    Added mismatched ID error message

--- Contracts/ErrorMessageContracts.cs
+++ Contracts/ErrorMessageContracts.cs
@@ -3,5 +3,6 @@
     public static class ErrorMessageContracts
     {
         public static string IdConflict = "An item with that ID already exists.";
+        public const string MismatchedId = "The ID in the URL does not match the ID of the item.";
     }
 }
