e87de5d2911d5ea0f2bf0581c40986b3080dc1b2
Andy Horn <andyjhorn@gmail.com>
2020-07-28 08:06:29 -0500

    Added static Contracts classes

---
+++ Contracts/ErrorMessageContracts.cs
@@ -0,0 +1,7 @@
+﻿namespace WeddingWebsiteCore.Contracts
+{
+    public static class ErrorMessageContracts
+    {
+        public static string IdConflict = "An item with that ID already exists.";
+    }
+}
---
+++ Contracts/RouteContracts.cs
@@ -0,0 +1,11 @@
+﻿namespace WeddingWebsiteCore.Contracts
+{
+    public static class RouteContracts
+    {
+        public static string GetAll = "";
+        public static string GetItem = "{id}";
+        public static string PostItem = "";
+        public static string PutItem = "{id}";
+        public static string DeleteItem = "{id}";
+    }
+}
