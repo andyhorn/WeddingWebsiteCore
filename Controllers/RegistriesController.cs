5188504f0300ec2a1d9d7a79d85a1f9d0db63466
Andy Horn <andyjhorn@gmail.com>
2020-07-29 07:47:23 -0500

    Finished implementing Images, Registries, and Rsvps controllers.

--- Controllers/RegistriesController.cs
+++ Controllers/RegistriesController.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using WeddingWebsiteCore.Contracts;
+using WeddingWebsiteCore.DataAccess;
 
 namespace WeddingWebsiteCore.Controllers
 {
@@ -11,5 +15,123 @@ namespace WeddingWebsiteCore.Controllers
     [ApiController]
     public class RegistriesController : ControllerBase
     {
+        private readonly WeddingContext _context;
+        public RegistriesController(WeddingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(RouteContracts.GetAll)]
+        public async Task<IActionResult> GetAllRegistries()
+        {
+            var registries = await _context.Registries.ToListAsync();
+
+            return Ok(registries);
+        }
+
+        [HttpGet(RouteContracts.GetItem)]
+        public async Task<IActionResult> GetRegistry(int id)
+        {
+            var registry = await _context.Registries.FindAsync(id);
+
+            if (registry == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(registry);
+        }
+
+        [HttpPost(RouteContracts.PostItem)]
+        public async Task<IActionResult> PostNewRegistry([FromBody]Models.Registry registry)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var exists = await ExistsAsync(registry.RegistryId);
+            if (exists)
+            {
+                return BadRequest(ErrorMessageContracts.IdConflict);
+            }
+
+            try
+            {
+                await _context.AddAsync(registry);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(PostNewRegistry), registry.RegistryId);
+        }
+
+        [HttpPut(RouteContracts.PutItem)]
+        public async Task<IActionResult> UpdateRegistry(int id, [FromBody]Models.Registry registry)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var exists = await ExistsAsync(registry.RegistryId);
+            if (!exists)
+            {
+                return NotFound();
+            }
+
+            if (id != registry.RegistryId)
+            {
+                return BadRequest(ErrorMessageContracts.MismatchedId);
+            }
+
+            try
+            {
+                _context.Entry(registry).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete(RouteContracts.DeleteItem)]
+        public async Task<IActionResult> DeleteRegistry(int id)
+        {
+            var exists = await ExistsAsync(id);
+            if (!exists)
+            {
+                return NotFound();
+            }
+
+            var item = await _context.Registries.FindAsync(id);
+
+            try
+            {
+                _context.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
+
+        private async Task<bool> ExistsAsync(int id)
+        {
+            var item = await _context.Registries.FindAsync(id);
+            return item != null;
+        }
     }
 }
--- Controllers/RsvpsController.cs
+++ Controllers/RsvpsController.cs
@@ -1,9 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WeddingWebsiteCore.Contracts;
+using WeddingWebsiteCore.DataAccess;
+using WeddingWebsiteCore.Models;
 
 namespace WeddingWebsiteCore.Controllers
 {
@@ -11,5 +13,124 @@ namespace WeddingWebsiteCore.Controllers
     [ApiController]
     public class RsvpsController : ControllerBase
     {
+        private readonly WeddingContext _context;
+        public RsvpsController(WeddingContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet(RouteContracts.GetAll)]
+        public async Task<IActionResult> GetAllRsvps()
+        {
+            var rsvps = await _context.Rsvps.ToListAsync();
+
+            return Ok(rsvps);
+        }
+
+        [HttpGet(RouteContracts.GetItem)]
+        public async Task<IActionResult> GetRsvp(int id)
+        {
+            var rsvp = await _context.Rsvps.FindAsync(id);
+
+            if (rsvp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(rsvp);
+        }
+
+        [HttpPost(RouteContracts.PostItem)]
+        public async Task<IActionResult> PostNewRsvp([FromBody]Rsvp rsvp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var exists = await ExistsAsync(rsvp.RsvpId);
+            if (exists)
+            {
+                return BadRequest(ErrorMessageContracts.IdConflict);
+            }
+
+            try
+            {
+                await _context.AddAsync(rsvp);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return CreatedAtAction(nameof(PostNewRsvp), rsvp.RsvpId);
+        }
+
+        [HttpPut(RouteContracts.PutItem)]
+        public async Task<IActionResult> UpdateRsvp(int id, [FromBody]Rsvp rsvp)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState.Values);
+            }
+
+            var exists = await ExistsAsync(id);
+            if (exists)
+            {
+                return BadRequest(ErrorMessageContracts.IdConflict);
+            }
+
+            if (id != rsvp.RsvpId)
+            {
+                return BadRequest(ErrorMessageContracts.MismatchedId);
+            }
+
+            try
+            {
+                _context.Entry(rsvp).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete(RouteContracts.DeleteItem)]
+        public async Task<IActionResult> DeleteRsvp(int id)
+        {
+            var exists = await ExistsAsync(id);
+            if (!exists)
+            {
+                return NotFound();
+            }
+
+            var rsvp = await _context.Rsvps.FindAsync(id);
+
+            try
+            {
+                _context.Remove(rsvp);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return NoContent();
+        }
+
+        private async Task<bool> ExistsAsync(int id)
+        {
+            var item = await _context.Rsvps.FindAsync(id);
+
+            return item != null;
+        }
     }
 }
