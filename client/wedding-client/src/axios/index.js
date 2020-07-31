bd8b1d7e2174ac53dc62cf6cba07cdba504b3733
Andy Horn <andyjhorn@gmail.com>
2020-07-30 20:16:09 -0500

    Fully installed and setup Axios

--- client/wedding-client/src/axios/index.js
+++ client/wedding-client/src/axios/index.js
@@ -2,8 +2,29 @@ import axios from "axios";
 
 const baseURL = process.env.VUE_APP_BASE_URL || "/api";
 
-const instance = axios.create({
-    baseURL
+const headers = {
+    'Content-Type': 'application/json'
+};
+
+const http = axios.create({
+    baseURL,
+    headers
 });
 
-export default instance;
+const addToken = function (token) {
+    if (!http.defaults.headers)
+        http.defaults.headers = headers;
+
+    http.defaults.headers["Authorization"] = `Bearer ${token}`;
+}
+
+const stripToken = function () {
+    if (http.defaults.headers && http.defaults.headers.Authorization)
+        http.defaults.headers.Authorization = undefined;
+}
+
+export {
+    http,
+    addToken,
+    stripToken
+};
--- client/wedding-client/src/store/index.js
+++ client/wedding-client/src/store/index.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { http, addToken, stripToken } from "@/axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+const defaultState = {
+
+};
 
 export default new Vuex.Store({
   state: {
