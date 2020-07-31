bb86f1af61f5d043c5e0b4ef6fcab535fa06a4f1
Andy Horn <andyjhorn@gmail.com>
2020-07-30 22:43:06 -0500

    Sticky header working

--- client/wedding-client/src/App.vue
+++ client/wedding-client/src/App.vue
@@ -1,11 +1,74 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>
+    <div :style="style">
+      <h1>Krystal &amp; Andy</h1>
+      <div :class="{sticky: isSticky}" ref="header">
+        <b-navbar>
+          <b-navbar-nav class="mx-auto">
+            <b-nav-item to="/">Home</b-nav-item>
+            <b-nav-item to>RSVP</b-nav-item>
+            <b-nav-item to>Photos</b-nav-item>
+            <b-nav-item to>Events</b-nav-item>
+            <b-nav-item to>Wedding Party</b-nav-item>
+            <b-nav-item to>Gift Registries</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+      </div>
     </div>
+    <hr />
     <router-view />
   </div>
 </template>
 
-<style>
+<script>
+import StickyHeader from "@/components/StickyHeader";
+
+export default {
+  name: "App",
+  components: {
+    StickyHeader
+  },
+  data() {
+    return {
+      isSticky: false,
+      style: {
+        paddingBottom: 0
+      },
+      stickyHeight: 0
+    };
+  },
+  mounted() {
+    const header = this.$refs.header;
+    const app = this.$refs.app;
+
+    this.stickyHeight = header.offsetTop;
+    console.log(this.stickyHeight);
+
+    const vm = this;
+    window.onscroll = function() {
+      if (window.pageYOffset > vm.stickyHeight) {
+        vm.isSticky = true;
+        vm.style.paddingBottom = vm.stickyHeight + 1 + "px";
+      } else {
+        vm.isSticky = false;
+        vm.style.paddingBottom = 0;
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+.header h1 {
+  font-size: 5rem;
+}
+.header a {
+  font-size: 1.5rem;
+}
+.sticky {
+  transition: 100ms;
+  position: fixed;
+  top: 0;
+  width: 100%;
+}
 </style>
--- client/wedding-client/src/views/Home.vue
+++ client/wedding-client/src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>This is some body text</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {}
+};
 </script>
+
+<style scoped>
+.home {
+  min-height: 150vh;
+}
+</style>
