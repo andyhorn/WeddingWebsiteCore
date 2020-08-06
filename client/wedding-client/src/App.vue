b0afff73812de0fefe7c3dce48a79295f6621367
Andy Horn <andyjhorn@gmail.com>
2020-08-05 22:04:15 -0500

    Added links to Admin dropdown menu

--- client/wedding-client/src/App.vue
+++ client/wedding-client/src/App.vue
@@ -4,20 +4,27 @@
       <h1 class="text-center pt-3">Krystal &amp; Andy</h1>
       <div :class="{sticky: isSticky}" class="border-bottom bg-light" ref="header">
         <b-navbar>
-          <b-navbar-nav class="mx-auto">
+          <b-navbar-nav class="mx-auto text-medium">
             <b-nav-item to="/">Home</b-nav-item>
             <b-nav-item to>RSVP</b-nav-item>
             <b-nav-item to>Photos</b-nav-item>
             <b-nav-item to>Events</b-nav-item>
             <b-nav-item to>Wedding Party</b-nav-item>
             <b-nav-item to>Gift Registries</b-nav-item>
-            <b-nav-item v-if="isLoggedIn" :to="{ name: 'Admin' }">Admin</b-nav-item>
+            <!-- <b-nav-item v-if="isLoggedIn" :to="{ name: 'Admin' }">Admin</b-nav-item> -->
+            <b-nav-item-dropdown v-if="isLoggedIn" id="nav-dropdown-admin" text="Admin" left>
+              <b-dropdown-item :to="{ name: 'Admin-Guests' }">Guests</b-dropdown-item>
+              <b-dropdown-item :to="{ name: 'Admin-RSVP' }">RSVPs</b-dropdown-item>
+              <b-dropdown-item :to="{ name: 'Admin-Photos' }">Photos</b-dropdown-item>
+              <b-dropdown-item :to="{ name: 'Admin-Events' }">Events</b-dropdown-item>
+              <b-dropdown-item :to="{ name: 'Admin-Party' }">Wedding Party</b-dropdown-item>
+            </b-nav-item-dropdown>
             <b-nav-item v-if="isLoggedIn" @click="logout">Logout</b-nav-item>
           </b-navbar-nav>
         </b-navbar>
       </div>
     </div>
-    <div class="pt-3 container">
+    <div>
       <router-view />
     </div>
   </div>
@@ -81,7 +88,7 @@ export default {
 .header h1 {
   font-size: 5rem;
 }
-.header a {
+.text-medium {
   font-size: 1.5rem;
 }
 .sticky {
--- client/wedding-client/src/views/Admin.vue
+++ client/wedding-client/src/views/Admin.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="container">
-    <h1>Admin</h1>
-  </div>
+  <div class="vh-100 container"></div>
 </template>
 
 <script>
 export default {
   name: "Admin",
   data() {
-    return {
-      loggedInStateWatcher: null
-    };
+    return {};
   },
-  mounted() {},
   methods: {}
 };
 </script>
+
+<style scoped>
+#admin-nav {
+  min-width: 100px;
+  max-width: 100px;
+  position: absolute;
+  left: 0;
+  top: 10rem;
+  z-index: -1;
+}
+
+#admin-display {
+  padding-left: 101px;
+}
+</style>
