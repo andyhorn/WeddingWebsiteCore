82ac2636a3d048c05486b41cab9a0f5ffdcd9def
Andy Horn <andyjhorn@gmail.com>
2020-09-11 09:57:46 -0500

    Removed the Address property from the Vendor model

--- Models/Vendor.cs
+++ Models/Vendor.cs
@@ -7,17 +7,21 @@ namespace WeddingWebsiteCore.Models
     public class Vendor
     {
         public int VendorId { get; set; }
+
+        [Required]
         public string Name { get; set; }
+
         public string Description { get; set; }
+
         [DataType(DataType.PhoneNumber)]
         public string ContactPhone { get; set; }
+
         [DataType(DataType.EmailAddress)]
         public string ContactEmail { get; set; }
+
         [DataType(DataType.Url)]
         public string Url { get; set; }
 
-        [ForeignKey(nameof(AddressId))]
-        public Address Address { get; set; }
-        public int AddressId { get; set; }
+        public int? AddressId { get; set; }
     }
 }
