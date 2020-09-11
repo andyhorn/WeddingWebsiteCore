bed941bae590806c1738ccbc3e5d5a67305366cb
Andy Horn <andyjhorn@gmail.com>
2020-09-11 12:52:03 -0500

    added RSVP support to Vuex Store

--- client/wedding-client/src/store/index.js
+++ client/wedding-client/src/store/index.js
@@ -5,6 +5,7 @@ import guests from "@/store/modules/guests.store.js";
 import families from "@/store/modules/families.store.js";
 import events from "@/store/modules/events.store.js";
 import addresses from "@/store/modules/addresses.store.js";
+import rsvps from "@/store/modules/rsvps.store.js";
 
 Vue.use(Vuex);
 
@@ -14,7 +15,8 @@ export const ACTIONS = {
   GUEST_ACTIONS: guests.GUEST_ACTIONS,
   EVENT_ACTIONS: events.EVENT_ACTIONS,
   AUTHENTICATION_ACTIONS: authentication.AUTHENTICATION_ACTIONS,
-  ADDRESS_ACTIONS: addresses.ACTIONS
+  ADDRESS_ACTIONS: addresses.ACTIONS,
+  RSVP_ACTIONS: rsvps.ACTIONS
 }
 
 const defaultState = {
@@ -40,7 +42,8 @@ export default new Vuex.Store({
     guests,
     families,
     events,
-    addresses
+    addresses,
+    rsvps
   },
   getters: {
 
---
+++ client/wedding-client/src/store/modules/rsvps.store.js
@@ -0,0 +1,134 @@
+import rsvpService from "@/services/rsvpService";
+
+const MUTATIONS = {
+    SET_RSVPS: "SET_RSVPS",
+    RESET_RSVPS: "RESET_RSVPS",
+    ADD_RSVP: "ADD_RSVP",
+    REMOVE_RSVP: "REMOVE_RSVP"
+};
+
+const ACTIONS = {
+    CREATE: "createRsvp",
+    UPDATE: "updateRsvp",
+    FETCH: "fetchRsvp",
+    FETCH_ALL: "fetchAllRsvps",
+    DELETE: "deleteRsvp"
+};
+
+const defaultState = {
+    rsvps: []
+};
+
+const initialState = () => defaultState;
+
+const state = initialState();
+
+const getters = {
+    rsvps: state => state.rsvps,
+    findRsvp: state => rsvpId => state.rsvps.find(x => x.rsvpId == rsvpId)
+};
+
+const actions = {
+    [ACTIONS.FETCH_ALL]({ commit }) {
+        return new Promise(async resolve => {
+            const rsvps = await rsvpService.getAll();
+
+            if (rsvps) {
+                commit(MUTATIONS.SET_RSVPS, rsvps);
+            }
+
+            resolve(rsvps != null);
+        });
+    },
+
+    [ACTIONS.FETCH]({ commit }, rsvpId) {
+        return new Promise(async resolve => {
+            const rsvp = await rsvpService.getOne(rsvpId);
+
+            if (rsvp) {
+                commit(MUTATIONS.ADD_RSVP, rsvp);
+            }
+
+            resolve(rsvp);
+        });
+    },
+
+    [ACTIONS.DELETE]({ commit }, rsvpId) {
+        return new Promise(async resolve => {
+            const deleted = await rsvpService.delete(rsvpId);
+
+            if (deleted) {
+                commit(MUTATIONS.REMOVE_RSVP, rsvpId);
+            }
+
+            resolve(deleted);
+        });
+    },
+
+    [ACTIONS.UPDATE]({ commit }, payload) {
+        return new Promise(async resolve => {
+            const updated = await rsvpService.update(payload.rsvpId, payload);
+
+            if (updated) {
+                const rsvp = await rsvpService.getOne(payload.rsvpId);
+                commit(MUTATIONS.ADD_RSVP, rsvp);
+            }
+
+            resolve(updated);
+        });
+    },
+
+    [ACTIONS.CREATE]({ commit }, payload) {
+        return new Promise(async resolve => {
+            const rsvpId = await rsvpService.create(payload);
+
+            if (rsvpId) {
+                const rsvp = await rsvpService.getOne(rsvpId);
+                commit(MUTATIONS.ADD_RSVP, rsvp);
+            }
+
+            resolve(rsvpId);
+        });
+    }
+};
+
+const mutations = {
+    [MUTATIONS.SET_RSVPS](state, rsvps) {
+        state.rsvps = rsvps;
+    },
+
+    [MUTATIONS.ADD_RSVP](state, rsvp) {
+        const rsvps = state.rsvps;
+        const index = state.rsvps.findIndex(x => x.rsvpId == rsvpId);
+
+        if (index == -1) {
+            rsvps.push(rsvp);
+        } else {
+            rsvps.splice(index, 1, rsvp);
+        }
+
+        state.rsvps = rsvps;
+    },
+
+    [MUTATIONS.REMOVE_RSVP](state, rsvpId) {
+        const rsvps = state.rsvps;
+        const index = rsvps.findIndex(x => x.rsvpId == rsvpId);
+
+        if (index != -1) {
+            rsvps.splice(index, 1);
+            state.rsvps = rsvps;
+        }
+    },
+
+    [MUTATIONS.RESET_RSVPS](state) {
+        state.rsvps = [];
+    }
+};
+
+export default {
+    ACTIONS,
+    state,
+    getters,
+    actions,
+    mutations
+}
