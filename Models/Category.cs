53549bcf088f3061f42eeee27a5a1bd1d0bc1549
Andy Horn <andyjhorn@gmail.com>
2020-10-13 18:51:02 -0500

    Added Accomodation and Category models

---
+++ Models/Category.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+[Table("categories")]
+public class Category
+{
+    [Key]
+    public int CategoryId { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [ForeignKey(nameof(ParentId))]
+    public Category Parent { get; set; }
+    public int ParentId { get; set; }
+}
