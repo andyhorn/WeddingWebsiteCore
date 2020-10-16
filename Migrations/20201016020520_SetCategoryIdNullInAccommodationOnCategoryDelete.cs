f23168d2bacc478a046b6709919b3f4830fcc529
Andy <andyjhorn@gmail.com>
2020-10-15 21:08:33 -0500

    Updated Accommodation model builder
    Set CategoryId and AddressId to null in Accommodations if either are deleted

---
+++ Migrations/20201016020520_SetCategoryIdNullInAccommodationOnCategoryDelete.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeddingWebsiteCore.Migrations
+{
+    public partial class SetCategoryIdNullInAccommodationOnCategoryDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_accommodations_addresses_AddressId",
+                table: "accommodations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_accommodations_categories_CategoryId",
+                table: "accommodations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_accommodations_addresses_AddressId",
+                table: "accommodations",
+                column: "AddressId",
+                principalTable: "addresses",
+                principalColumn: "AddressId",
+                onDelete: ReferentialAction.SetNull);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_accommodations_categories_CategoryId",
+                table: "accommodations",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_accommodations_addresses_AddressId",
+                table: "accommodations");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_accommodations_categories_CategoryId",
+                table: "accommodations");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_accommodations_addresses_AddressId",
+                table: "accommodations",
+                column: "AddressId",
+                principalTable: "addresses",
+                principalColumn: "AddressId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_accommodations_categories_CategoryId",
+                table: "accommodations",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
