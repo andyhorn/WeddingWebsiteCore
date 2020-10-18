b5ba7f25f37f4dd02ac9b596e8a55e0a0d33a7a0
Andy <andyjhorn@gmail.com>
2020-10-18 12:07:49 -0500

    Implementing GuestForm
    Created the GuestForm and implemented it into the NewGuestModal

---
+++ client/src/components/forms/GuestForm.vue
@@ -0,0 +1,190 @@
+<template>
+    <b-form @submit.prevent="onSubmit">
+        <b-row>
+            <b-col>
+                <b-form-group label="First Name" :state="firstNameState" description="Required">
+                    <b-form-input v-model="firstName" :state="firstNameState" />
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-form-group label="Last Name" :state="lastNameState" description="Required">
+                    <b-form-input v-model="lastName" :state="lastNameState" />
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-form-group label="Family">
+                    <b-select v-model="familyId">
+                        <option :value="null">None</option>
+                        <option v-for="family in families"
+                            :key="family.familyId"
+                            :value="family.familyId"
+                        >
+                            {{ family.name }}
+                        </option>
+                    </b-select>
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col>
+                <b-form-group label="Is a Wedding Party Member?">
+                    <b-form-checkbox switch v-model="isWeddingMember" />
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col cols="4" class="d-flex flex-column align-items-start justify-content-around">
+                <b-form-group label="Is a Child?">
+                    <b-form-checkbox switch v-model="isChild" />
+                </b-form-group>
+                <b-form-group label="Is Under Ten (10)?" v-if="isChild">
+                    <b-form-checkbox switch v-model="isUnderTen" />
+                </b-form-group>
+            </b-col>
+            <b-col v-if="isChild">
+                <b-form-group label="Parent" :state="parentState">
+                    <b-form-select v-model="parentId" :state="parentState">
+                        <option :value="null">None</option>
+                        <option v-for="parent in parents"
+                            :key="parent.guestId"
+                            :value="parent.guestId"
+                        >
+                            {{ `${parent.firstName} ${parent.lastName}` }}
+                        </option>
+                    </b-form-select>
+                </b-form-group>
+            </b-col>
+        </b-row>
+        <b-row>
+            <b-col class="d-flex justify-content-between">
+                <b-button squared size="sm" variant="success" type="submit" :disabled="!isFormValid">Save</b-button>
+                <b-button squared size="sm" variant="danger" @click="onCancel">Cancel</b-button>
+            </b-col>
+        </b-row>
+    </b-form>
+</template>
+
+<script>
+import { ACTIONS } from "@/store";
+const Toast = require("@/helpers/toast");
+
+export default {
+    name: "GuestForm",
+    props: [ "guest" ],
+    data() {
+        return {
+            id: null,
+            firstName: null,
+            lastName: null,
+            familyId: null,
+            isChild: false,
+            isUnderTen: false,
+            isWeddingMember: false,
+            parentId: null,
+            firstNameState: null,
+            lastNameState: null,
+            parentState: null
+        }
+    },
+    computed: {
+        isFormValid() {
+            return this.firstNameState
+                && this.lastNameState
+                && (this.isChild && this.parentState || !this.isChild && this.parentState == null);
+        },
+        families() {
+            return this.$store.getters.families;
+        },
+        parents() {
+            return this.$store.getters.nonChildren;
+        }
+    },
+    watch: {
+        "firstName": function () {
+            this.firstNameState = !!this.firstName && !!this.firstName.trim();
+        },
+        "lastName": function () {
+            this.lastNameState = !!this.lastName && !!this.lastName.trim();
+        },
+        "parentId": function () {
+            if (this.isChild)
+                this.parentState = this.parentId != null;
+            else
+                this.parentState = null;
+        },
+        "guest": {
+            immediate: true,
+            deep: true,
+            handler: function () {
+                if (this.guest == null) return;
+
+                this.id = this.guest.guestId;
+                this.firstName = this.guest.firstName;
+                this.lastName = this.guest.lastName;
+                this.familyId = this.guest.familyId;
+                this.isChild = this.guest.isChild;
+                this.isUnderTen = this.guest.isUnderTen;
+                this.isWeddingMember = this.guest.isWeddingMember;
+                this.parentId = this.guest.parentId;
+            }
+        }
+    },
+    methods: {
+        clear() {
+            this.id = null;
+            this.firstName = null;
+            this.lastName = null;
+            this.familyId = null;
+            this.isChild = false;
+            this.isUnderTen = false;
+            this.isWeddingMember = false;
+            this.parentId = null;
+        },
+        resetStates() {
+            this.firstNameState = null;
+            this.lastNameState = null;
+            this.parentState = null;
+        },
+        close(id) {
+            this.$emit("close", id);
+            this.clear();
+            this.resetStates();
+        },
+        onCancel() {
+            this.close(null);
+        },
+        async onSubmit() {
+            if (!this.isFormValid) return;
+
+            const guest = {
+                guestId: this.id || undefined,
+                firstName: this.firstName,
+                lastName: this.lastName,
+                familyId: this.familyId,
+                isChild: this.isChild,
+                isUnderTen: this.isUnderTen,
+                isWeddingMember: this.isWeddingMember,
+                parentId: this.parentId
+            };
+
+            const command = this.id == null
+                ? ACTIONS.GUEST_ACTIONS.CREATE
+                : ACTIONS.GUEST_ACTIONS.UPDATE;
+
+            const success = await this.$store.dispatch(command, guest);
+            if (success) {
+                const refresh = ACTIONS.GUEST_ACTIONS.FETCH_ALL;
+                await this.$store.dispatch(refresh);
+                Toast.success("Guest saved!");
+                this.close(success);
+            } else {
+                Toast.error("Unable to save guest.");
+            }
+        }
+    }
+}
+</script>
--- client/src/components/modals/NewGuestModal.vue
+++ client/src/components/modals/NewGuestModal.vue
@@ -1,216 +1,36 @@
 <template>
   <b-modal
     :visible="visible"
+    v-if="visible"
     id="new-guest-modal"
     title="Add New Guest"
-    @show="onOpen"
-    @hide="onCancel"
     hide-footer
   >
-    <form @submit.prevent="onSubmit" :disabled="isBusy">
-      <b-container>
-        <b-row>
-          <b-col>
-            <b-form-group
-              id="first-name-input-group"
-              label="First Name"
-              label-for="first-name-input"
-              invalid-feedback="First name is required."
-              :state="firstNameState"
-            >
-              <b-form-input
-                id="first-name-input"
-                v-model="firstName"
-                :state="firstNameState"
-                @input="onFirstNameInput"
-                trim
-                required
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-form-group
-              id="last-name-input-group"
-              label="Last Name"
-              label-for="last-name-input"
-              invalid-feedback="Last name is required."
-              :state="lastNameState"
-            >
-              <b-form-input
-                id="last-name-input"
-                v-model="lastName"
-                :state="lastNameState"
-                @input="onLastNameInput"
-                trim
-                required
-              />
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-form-group id="family-select-group" label="Family" label-for="family-select">
-              <b-select v-model="familyId" :disabled="families.length == 0">
-                <b-select-option
-                  v-for="family in families"
-                  :key="family.familyId"
-                  :value="family.familyId"
-                >{{ family.name }}</b-select-option>
-              </b-select>
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <b-row>
-          <b-col>
-            <b-row>
-              <b-col>
-                <b-form-group
-                  id="is-child-switch-group"
-                  label="Is a Child"
-                  label-for="is-child-switch"
-                >
-                  <b-form-checkbox id="is-child-switch" v-model="isChild" switch />
-                </b-form-group>
-              </b-col>
-              <transition name="fade-in">
-                <b-col v-if="isChild">
-                  <b-form-group
-                    id="is-under-ten-switch-group"
-                    label="Under Ten"
-                    label-for="is-under-ten-switch"
-                  >
-                    <b-form-checkbox switch id="is-under-ten-switch" v-model="isUnderTen" />
-                  </b-form-group>
-                </b-col>
-              </transition>
-            </b-row>
-          </b-col>
-          <b-col>
-            <b-form-group
-              id="wedding-member-switch-group"
-              label="Is a Wedding Party member"
-              label-for="wedding-member-switch"
-            >
-              <b-form-checkbox switch id="wedding-member-switch" v-model="isWeddingMember" />
-            </b-form-group>
-          </b-col>
-        </b-row>
-        <transition name="fade-in">
-          <b-row v-if="isChild">
-            <b-col>
-              <b-form-group label="Parent">
-                <b-select v-model="parentId">
-                  <option v-for="guest in guests" :key="guest.guestId" :value="guest.guestId">
-                    <span>{{ guest.firstName }} {{ guest.lastName }}</span>
-                  </option>
-                </b-select>
-              </b-form-group>
-            </b-col>
-          </b-row>
-        </transition>
-      </b-container>
-      <b-button type="Submit" :disabled="isBusy">
-        <b-spinner small v-if="isBusy" />
-        <span v-else>Save</span>
-      </b-button>
-    </form>
+    <GuestForm @close="onGuestFormClose" />
   </b-modal>
 </template>
 
 <script>
 import { ACTIONS } from "@/store";
+import GuestForm from "@/components/forms/GuestForm";
 const Toast = require("@/helpers/toast");
 
 export default {
   name: "NewGuestModal",
   props: ["families", "selectedFamilyId", "visible"],
+  components: {
+    GuestForm
+  },
   data() {
     return {
-      firstName: "",
-      lastName: "",
-      isChild: false,
-      isUnderTen: false,
-      parentId: null,
-      isWeddingMember: false,
-      familyId: null,
-      firstNameState: null,
-      lastNameState: null,
-      isBusy: false,
     };
   },
   computed: {
-    guests() {
-      return this.$store.getters.nonChildren;
-    },
   },
   methods: {
-    onOpen() {
-      if (this.selectedFamilyId) {
-        this.familyId = this.selectedFamilyId;
-      }
-    },
-    onCancel() {
-      this.close();
-    },
-    async onSubmit() {
-      this.isBusy = true;
-      const guest = {
-        firstName: this.firstName,
-        lastName: this.lastName,
-        isChild: this.isChild,
-        isUnderTen: this.isChild ? this.isUnderTen : false,
-        isWeddingMember: this.isWeddingMember,
-        familyId: this.familyId,
-        parentId: this.isChild ? this.parentId : null,
-      };
-
-      const guestId = await this.$store.dispatch(ACTIONS.GUEST_ACTIONS.CREATE, guest);
-
-      if (!guestId) {
-        Toast.error("Unable to save new guest.");
-        return;
-      }
-
-      if (guest.familyId) {
-        await this.$store.dispatch(
-          ACTIONS.FAMILY_ACTIONS.FETCH,
-          guest.familyId
-        );
-      }
-
-      Toast.success("New guest saved!");
-
-      this.close();
-    },
-    onFirstNameInput() {
-      if (this.firstName && this.firstName.length) {
-        this.firstNameState = true;
-      } else {
-        this.lastNameState = false;
-      }
-    },
-    onLastNameInput() {
-      if (this.lastName && this.lastName.length) {
-        this.lastNameState = true;
-      } else {
-        this.lastNameState = false;
-      }
-    },
-    close() {
-      this.firstName = "";
-      this.lastName = "";
-      this.isChild = false;
-      this.isUnderTen = false;
-      this.parentId = null;
-      this.isWeddingMember = false;
-      this.familyId = null;
-      this.firstNameState = null;
-      this.lastNameState = null;
-      this.isBusy = false;
-      this.$emit("closed");
-    },
+    onGuestFormClose(id) {
+      this.$emit("close", id);
+    }
   },
 };
 </script>
